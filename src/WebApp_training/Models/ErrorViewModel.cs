8ebcd30c4c5e3327a1ba44f31f62e6c130b41a2c
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-19 16:48:55 +0900

    srcFirstCommit

---
+++ src/WebApp_training/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApp_training.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
