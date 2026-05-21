22ed079cc55e5084762033ae1805a05c8c4ef471
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-21 09:49:21 +0900

    Entity作成

---
+++ src/WebApp_training/Infrastructures/Entities/DepartmentEntity.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_training.Infrastructures.Entities;
+
+[Table("department")]
+public class DepartmentEntity
+{
+    [Key]
+    [Column("id")]
+    public int DeptId { get; set; }
+
+    [Column("name")]
+    public string DeptName { get; set; } = string.Empty;
+}
---
+++ src/WebApp_training/Infrastructures/Entities/EmployeeEntity.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_training.Infrastructures.Entities;
+
+[Table("employee")]
+public class EmployeeEntity
+{
+    [Key]
+    [Column("id")]
+    public int EmpId { get; set; }
+
+    [Column("name")]
+    public string EmpName { get; set; } = string.Empty;
+
+    [Column("dept_id")]
+    public int? DeptId { get; set; }
+
+    [Column("phone_number")]
+    public string? PhoneNum { get; set; }
+
+    [Column("e_mail")]
+    public string? EMail { get; set; }
+}
