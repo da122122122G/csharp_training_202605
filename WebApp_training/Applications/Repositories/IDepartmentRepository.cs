48f863974ba7e02dc77ac03201b0b437cf449132
da122122122G <daichi.712.houzuki@gmail.com>
2026-05-21 11:10:55 +0900

    微修正

--- WebApp_training/Applications/Repositories/IDepartmentRepository.cs
+++ WebApp_training/Applications/Repositories/IDepartmentRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebApp_training.Applications.Domains;
 
-namespace WebApp_training.Applications.Repositries;
+namespace WebApp_training.Applications.Repositories;
 
 public interface IDepartmentRepository
 {
